b444275ae94582be0a1083fa5587bd569bbeccfa
parakhatdin <nuratdinov.p@gmail.com>
2020-09-23 16:06:52 +0500

    created notification TextPost and install telegram-notification plugin

---
+++ app/Notifications/TextPost.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramChannel;
+
+class TextPost extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [TelegramChannel::class];
+    }
+
+    public function toTelegram($notifiable)
+    {
+        return TelegramMessage::create()
+            // Optional recipient user id.
+            ->to($notifiable->telegram_user_id)
+            // Markdown supported.
+            ->content("Hello there!\nYour invoice has been *PAID*");
+    }
+}
